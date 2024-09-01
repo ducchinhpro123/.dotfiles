c2e37f100e4c3a8be51598d4fcc3f1612879d7a1
ducchinhpro123 <chinh99562@donga.edu.vn>
2024-09-01 19:02:17 +0700

    init

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,73 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('bufferline').setup {
+        options = {
+          mode = 'buffers', -- set to "tabs" to only show tabpages instead
+          numbers = 'none', -- can be "none" | "ordinal" | "buffer_id" | "both"
+          close_command = 'bdelete! %d', -- can be a string | function, see "Mouse actions"
+          right_mouse_command = 'bdelete! %d', -- can be a string | function, see "Mouse actions"
+          left_mouse_command = 'buffer %d', -- can be a string | function, see "Mouse actions"
+          middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+          indicator = {
+            icon = '▎', -- this should be omitted if indicator style is not 'icon'
+            style = 'icon', -- can be 'icon' | 'underline' | 'none',
+          },
+          buffer_close_icon = '󰅖',
+          modified_icon = '●',
+          close_icon = '',
+          left_trunc_marker = '',
+          right_trunc_marker = '',
+          max_name_length = 18,
+          max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+          truncate_names = true, -- whether or not tab names should be truncated
+          tab_size = 18,
+          diagnostics = 'nvim_lsp',
+          diagnostics_update_in_insert = false,
+          offsets = {
+            {
+              filetype = 'NvimTree',
+              text = 'File Explorer',
+              text_align = 'left',
+              separator = true,
+            },
+          },
+          color_icons = true, -- whether or not to add the filetype icon highlights
+          show_buffer_icons = true, -- disable filetype icons for buffers
+          show_buffer_close_icons = true,
+          show_close_icon = true,
+          show_tab_indicators = true,
+          persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+          separator_style = 'thin', -- can be "slant" | "thick" | "thin" | { 'any', 'any' },
+          enforce_regular_tabs = false,
+          always_show_bufferline = true,
+          hover = {
+            enabled = true,
+            delay = 200,
+            reveal = { 'close' },
+          },
+        },
+      }
+
+      -- Keymappings for buffer navigation
+      vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+    end,
+  },
+  {
+    'akinsho/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
+  },
+}
