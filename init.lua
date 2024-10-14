8a7cda5f6aa25810b44a18ee42b222698fc18922
ducchinhpro123 <chinh99562@donga.edu.vn>
2024-10-14 19:52:43 +0700

    Add more options when compile latex

--- init.lua
+++ init.lua
@@ -1106,6 +1106,15 @@ vim.g.vimtex_syntax_enabled = 1
 vim.g.vimtex_view_general_viewer = 'zathura'
 vim.g.vimtex_view_general_options = '--unique file:@pdf#src:@line@tex'
 vim.g.vimtex_compiler_method = 'latexmk'
+vim.g.vimtex_compiler_latexmk = {
+  options = {
+    '-shell-escape',
+    '-verbose',
+    '-file-line-error',
+    '-synctex=1',
+    '-interaction=nonstopmode',
+  },
+}
 
 -- vim.maplocalleader = ","
 -- cmp.setup {
