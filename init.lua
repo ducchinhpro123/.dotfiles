1d9410fefc03700a0e66d500387a729e1cc2e320
ducchinhpro123 <chinh99562@donga.edu.vn>
2024-10-15 07:27:23 +0700

    Configure formatter for java

--- init.lua
+++ init.lua
@@ -740,10 +740,11 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         html = { 'prettierd', 'prettier', stop_after_first = true },
+        java = { 'google-java-format' },
         -- javascript = {},
       },
     },
